174beeeeb9aed36fd5158a28fc2cd0b3c1a72b87
Daniel X. Moore <yahivin@gmail.com>
2015-08-02 13:45:08 -0700

    Mail configuration

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -62,8 +62,16 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_url_options = { :host => "staging.pixieengine.com" }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address => "smtp.mandrillapp.com",
+    :port => "587",
+    :user_name => "yahivin@gmail.com",
+    :domain => "pixieengine.com"
+  }
+
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
