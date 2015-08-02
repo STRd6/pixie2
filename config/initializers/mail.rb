f1e9100ee117293723135dc1efd5ba166211c125
Daniel X. Moore <yahivin@gmail.com>
2015-08-02 13:53:01 -0700

    Loading mail config in staging

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -1,6 +1,6 @@
 Mail.register_interceptor(DevelopmentMailInterceptor) if Rails.env.staging?
 
-if Rails.env.production?
+if Rails.env.production? or Rails.env.staging?
   ActionMailer::Base.smtp_settings = ActionMailer::Base.smtp_settings.merge({
     :password => Rails.application.secrets.mandrill_api_key
   })
